0641015beb4916b7c707292cb1374977c1d37310
Andhika Yoga Perdana <36915696+andhikayp@users.noreply.github.com>
2019-02-22 17:38:57 +0700

    Update soal5.sh

--- soal5.sh
+++ soal5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk '/cron/ || /CRON/,!/sudo/' /var/log/syslog | awk 'NF<=12' >> /home/andhika/modul1/no_5_sisop.log
+awk '/cron/ || /CRON/ && !/sudo/ && !/SUDO/' /var/log/syslog | awk 'NF<=12' >> /home/andhika/modul1/no_5_sisop.log
