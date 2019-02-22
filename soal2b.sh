ed92fde9bad10227a321fe542ba67c8bfc0a31b3
Andhika Yoga Perdana <36915696+andhikayp@users.noreply.github.com>
2019-02-22 17:28:05 +0700

    Update and rename 2b.sh to soal2b.sh

--- soal2b.sh
+++ soal2b.sh
@@ -1,1 +1,3 @@
+#!/bin/bash 
+
 awk -F, '{if($7 == '2012' && $1=="United States") iter[$4]+=$10} END {for(hasil in iter) {print iter[hasil], hasil}}' WA_Sales_Products_2012-14.csv OFS=',' | sort -nr | head -n3 | awk -F, '{print $2}'
