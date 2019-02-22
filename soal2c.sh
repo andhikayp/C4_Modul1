01d678ab8acd44ae969866f32d355b06bf20852c
Andhika Yoga Perdana <36915696+andhikayp@users.noreply.github.com>
2019-02-22 17:28:47 +0700

    Update and rename 2c.sh to soal2c.sh

--- soal2c.sh
+++ soal2c.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 awk -F, '{if(($7 == '2012' && $1=="United States")  && ($4 == "Personal Accessories" || $4 == "Camping Equipment" || $4 == "Outdoor Protection")) iter[$6]+=$10} END {for(hasil in iter) {print iter[hasil], hasil}}' WA_Sales_Products_2012-14.csv OFS=',' | sort -nr | head -n3 | awk -F, '{print $2}'
